cce8b714b80270a8056c6ee4a1165da1b36a1034
unknown <jananijanani81796@gmail.com>
2026-03-18 01:28:04 +0530

    push new changes

--- NOTICE_BOARD_APP/digital_notice_board/settings.py
+++ NOTICE_BOARD_APP/digital_notice_board/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "digital-notice-board-b7ia.onrender.com",
+    "onrender.com",
+]
 
 # CSRF trusted origins for browser preview
 CSRF_TRUSTED_ORIGINS = [
